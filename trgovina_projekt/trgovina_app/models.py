167f47d7c241ae156ceceab80cce9df079e796f0
3.12 <3.12@gmail.si>
2026-05-21 15:30:52 +0200

    testni commit s

--- trgovina_projekt/trgovina_app/models.py
+++ trgovina_projekt/trgovina_app/models.py
@@ -1,17 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Izdelek(models.Model):
+class Oglas(models.Model):
+
+    TIPI_OGLASOV = {
+        "K" : "Kupim",
+        "P" : "Prodajam"
+    }
+
+    naslov = models.CharField(max_length=200, verbose_name="Naslov oglasa", help_text="Do 200 znakov")
+    opis = models.CharField(max_length=5000, blank=True, default="", verbose_name="Naslov oglasa", help_text="Do 5000 znakov")
+    lastnik = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Lastnik oglasa")
+    datum_cas = models.DateTimeField(auto_now_add=True, verbose_name="Datum in čas objave oglasa")
+    cena = models.DecimalField(decimal_places=2, verbose_name="Cena", help_text="Cena izdelka (v EUR)")
+    ogledi = models.IntegerField(default=0, verbose_name="Število ogledov oglasa")
+    Tip = models.CharField(max_length=20, choices=TIPI_OGLASOV.items())
+    kategorije = models.ManyToManyField("Kategorija", verbose_name="Kategorije, v katere spada oglas")
     
     class Meta:
-        verbose_name_plural = 'Izdelki'
+        verbose_name_plural = "Oglasi"
 
 class Kategorija(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Kategorije'
+        verbose_name_plural = "Kategorije"
 
 class Transakcija(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Transakcije'
+        verbose_name_plural = "Transakcije"
         
